55cb080228c3b44a500cb14b77f12f522c5fcf6c
levblanc <levblanc@gmail.com>
2022-09-06 18:52:28 +0800

    Add test script

---
+++ test/test-deploy.js
@@ -0,0 +1,26 @@
+const { assert } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('SimpleStorage', () => {
+  let simpleStorageFactory, simpleStorage;
+
+  beforeEach(async () => {
+    simpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
+    simpleStorage = await simpleStorageFactory.deploy();
+  });
+
+  it('Should start with a fav num of 0', async () => {
+    const currentValue = await simpleStorage.retrieve();
+    const expectedValue = '0';
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+
+  it('Should update when we call store()', async () => {
+    const expectedValue = '7';
+    const transactionResponse = await simpleStorage.store(expectedValue);
+    await transactionResponse.wait(1);
+
+    const updatedValue = await simpleStorage.retrieve();
+    assert.equal(updatedValue.toString(), expectedValue);
+  });
+});
