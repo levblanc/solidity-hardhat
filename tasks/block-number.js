f89347c2b4fa85ccf7d73b267405903041e0e437
levblanc <levblanc@gmail.com>
2022-09-06 18:52:21 +0800

    Add sample task

---
+++ tasks/block-number.js
@@ -0,0 +1,13 @@
+const { task } = require('hardhat/config');
+
+// Run task with default hardhat network:
+// yarn hardhat block-number
+
+// Run task with custom network:
+// yarn hardhat block-number --network <your-choice-of-network>
+task('block-number', 'Prints the block number').setAction(
+  async (taskArgs, hre) => {
+    const blockNum = await hre.ethers.provider.getBlockNumber();
+    console.log(`Current block number: ${blockNum}`);
+  }
+);
